c6e498aed18b65862f4ee1103bdf268b9fbbaa70
chiranshu khandelwal <chiranshumain2gmail.com>
2023-01-18 13:58:11 +0530

    feat: add spare frame calculation

--- bowlingPointsCalculator.js
+++ bowlingPointsCalculator.js
@@ -1,14 +1,18 @@
 function getBowlingPoints(input){
     let score = 0
     for(let i=0; i<input.length-1; i=i+2){
+        
+        // strike
         if(input[i] == 10){
             score = score + 10 + input[i+1] + input[i+2]
         }
 
+        // spare frame
         else if(input[i]+input[i+1] == 10){
-            score = score + 10 + input[i+1]
+            score = score + 10 + input[i+2]
         }
 
+        // open frame
         else{
             score = score + input[i] + input[i+1]
         }
@@ -18,5 +22,6 @@ function getBowlingPoints(input){
 }
 
 console.log( getBowlingPoints([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]) )
+console.log( getBowlingPoints([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) )
 
 module.exports = getBowlingPoints
