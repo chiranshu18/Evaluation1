b2abc2d0b679068ec90b0ba6da91c758359437f5
chiranshu khandelwal <chiranshumain2gmail.com>
2023-01-18 12:42:35 +0530

    chore: add tests

--- bowlingPointsCalculator.test.js
+++ bowlingPointsCalculator.test.js
@@ -1,1 +1,13 @@
 const getBowlingPoints = require('/.bowlingPointsCalculator')
+
+describe('Caclculate the points of a bowling game', ()=>{
+    it('should return the final score when all frames get less than 10 points', ()=>{
+        expect(getBowlingPoints([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toEqual(90)
+    })
+
+    it('should return 90 when spare is also present in frames', ()=>{
+        expect(getBowlingPoints([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toEqual(90)
+    })
+
+    
+})
